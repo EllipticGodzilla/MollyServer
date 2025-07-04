345a6664e69b5b6373feeb0fe1294ed74aa2f76f
EllipticGodzilla <camillo.zamponi@gmail.com>
2025-07-04 16:49:27 +0200

    updated the initialization of Logger

--- src/files/Logger.java
+++ src/files/Logger.java
@@ -1,43 +1,26 @@
 package files;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public abstract class Logger {
-    private static final FileOutputStream ostream;
+    private static FileOutputStream ostream;
     private static final SimpleDateFormat date_format = new SimpleDateFormat("[HH:mm:ss.SSS] - ");
 
-    static {
-        FileOutputStream temp_ostream;
-
-        //prova a creare l'output stream per il file, se non esiste ancora cerca di crearlo e riprova a inizializzare l'output stream
+    protected static void init() {
         try {
-            temp_ostream = new FileOutputStream(FileInterface.jar_path + "/database/TerminalLog.dat");
-        }
-        catch (FileNotFoundException _) {
-            try {
-                new File(FileInterface.jar_path + "/database/TerminalLog.dat").createNewFile();
-                temp_ostream = new FileOutputStream(FileInterface.jar_path + "/database/TerminalLog.dat");
-            }
-            catch (IOException e) {
-                System.out.println("impossibile creare il file per i log a: " + FileInterface.jar_path + "/database/TerminalLog.dat");
-                temp_ostream = null;
+            ostream = new FileOutputStream(FileInterface.jar_path + "/database/TerminalLog.dat");
+            ostream.write("clear\n".getBytes());
 
-                System.exit(0);
-            }
+            SimpleDateFormat start_format = new SimpleDateFormat("[yy/LL/dd]");
+            ostream.write(("================================== Server Started " + start_format.format(Calendar.getInstance()) + " ==================================").getBytes());
         }
-
-        //se è stato creato FileOutputStream con successo ha resettato il file, come prima cosa scrive "clear" per indicare che non è cifrato
-        try {
-            temp_ostream.write("clear\n".getBytes());
+        catch (Exception e) {
+            System.out.println("impossibile inizializzare il logger, " + e.getMessage());
+            System.exit(0);
         }
-        catch (IOException _) {}
-
-        ostream = temp_ostream;
     }
 
     public static void log(String txt) {
